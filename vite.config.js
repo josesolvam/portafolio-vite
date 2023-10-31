f786ee3c9e2d5b7256c4e063e8455d8d6fa619fb
josesolvam <joseramon.heredia@solvam.es>
2023-11-01 00:16:26 +0100

    Actualizada la carpeta de distribución docs para poder desplegarlo en Github Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/portafolio/",
   root: "src",
   build: {
     outDir: "../docs",
